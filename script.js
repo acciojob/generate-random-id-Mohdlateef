37e435fca9fe960fb772e68f4749f2f1da109e4b
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-01-19 16:13:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+const Charecters='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 function makeid(l) {
   // write your code here
+	let result=' ';
+	const charlength=Charecters.length;
+	for(let i=0;i<l;i++)
+		{
+			result+=Charecters.charAt(Math.floor(Math.random()*charlength));
+		}
+	return result;
+	
 }
 
 // Do not change the code below.
